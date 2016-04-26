eb3443c70acbfd95691d6bc5fff41602a1a2a567
Arjun <arjun@codebrahma.com>
2016-04-26 11:00:12 +0530

    base page swipe added

---
+++ library/src/main/java/in/arjsna/swipecardlib/FlingPageListener.java
@@ -0,0 +1,305 @@
+
+package in.arjsna.swipecardlib;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
+
+/**
+ * Created by arjun on 4/25/16.
+ */
+public class FlingPageListener implements View.OnTouchListener{
+    private static final String TAG = "FlingPageListener";
+    private static final int INVALID_POINTER_ID = -1;
+    private final Rect RECT_BOTTOM;
+    private final Rect RECT_TOP;
+    private final Rect RECT_RIGHT;
+    private final Rect RECT_LEFT;
+    private final View frame;
+    private final float objectX;
+    private final float objectY;
+    private final int objectH;
+    private final int objectW;
+    private final float halfWidth;
+    private final float halfHeight;
+    private final Object dataObject;
+    private final int parentWidth;
+    private final int parentHeight;
+    private final FlingListener mFlingListener;
+    private int mActivePointerId;
+    private float aDownTouchX;
+    private float aDownTouchY;
+    private float aPosX;
+    private float aPosY;
+    private boolean isAnimationRunning = false;
+
+//    public FlingPageListener(View frame, Object itemAtPosition, FlingListener flingListener) {
+//        this(frame, itemAtPosition, 15f, flingListener);
+//    }
+
+    public FlingPageListener(View frame, Object itemAtPosition, FlingListener flingListener) {
+        super();
+        this.frame = frame;
+        this.objectX = frame.getX();
+        this.objectY = frame.getY();
+        this.objectH = frame.getHeight();
+        this.objectW = frame.getWidth();
+        this.halfWidth = objectW / 2f;
+        this.halfHeight = objectH / 2f;
+        this.dataObject = itemAtPosition;
+        this.parentWidth = ((ViewGroup) frame.getParent()).getWidth();
+        this.parentHeight = ((ViewGroup) frame.getParent()).getHeight();
+        this.mFlingListener = flingListener;
+        this.RECT_TOP = new Rect((int) Math.max(frame.getLeft(), leftBorder()), 0, (int) Math.min(frame.getRight(), rightBorder()), (int) topBorder());
+        this.RECT_BOTTOM = new Rect((int) Math.max(frame.getLeft(), leftBorder()), (int) bottomBorder(), (int) Math.min(frame.getRight(), rightBorder()), parentHeight);
+        this.RECT_LEFT = new Rect(0, (int) Math.max(frame.getTop(), topBorder()), (int) leftBorder(), (int) Math.min(frame.getBottom(), bottomBorder()));
+        this.RECT_RIGHT = new Rect((int) rightBorder(), (int) Math.max(frame.getTop(), topBorder()), parentWidth, (int) Math.min(frame.getBottom(), bottomBorder()));
+    }
+
+    public float leftBorder() {
+        return parentWidth / 4.f;
+    }
+
+    public float rightBorder() {
+        return 3 * parentWidth / 4.f;
+    }
+
+    public float bottomBorder() {
+        return 3 * parentHeight / 4.f;
+    }
+
+    public float topBorder() {
+        return parentHeight  / 4.f;
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+
+                // from http://android-developers.blogspot.com/2010/06/making-sense-of-multitouch.html
+                // Save the ID of this pointer
+
+                mActivePointerId = event.getPointerId(0);
+                float x = 0;
+                float y = 0;
+                boolean success = false;
+                try {
+                    x = event.getX(mActivePointerId);
+                    y = event.getY(mActivePointerId);
+                    success = true;
+                } catch (IllegalArgumentException e) {
+                    Log.w(TAG, "Exception in onTouch(view, event) : " + mActivePointerId, e);
+                }
+                if (success) {
+                    // Remember where we started
+                    aDownTouchX = x;
+                    aDownTouchY = y;
+                    //to prevent an initial jump of the magnifier, aposX and aPosY must
+                    //have the values from the magnifier frame
+                    if (aPosX == 0) {
+                        aPosX = frame.getX();
+                    }
+                    if (aPosY == 0) {
+                        aPosY = frame.getY();
+                    }
+
+//                    if (y < objectH / 2) {
+//                        touchPosition = TOUCH_ABOVE;
+//                    } else {
+//                        touchPosition = TOUCH_BELOW;
+//                    }
+                }
+
+                view.getParent().requestDisallowInterceptTouchEvent(true);
+                break;
+
+            case MotionEvent.ACTION_UP:
+                mActivePointerId = INVALID_POINTER_ID;
+                resetCardViewOnStack();
+                view.getParent().requestDisallowInterceptTouchEvent(false);
+                break;
+
+            case MotionEvent.ACTION_POINTER_DOWN:
+                break;
+
+            case MotionEvent.ACTION_POINTER_UP:
+                // Extract the index of the pointer that left the touch sensor
+                final int pointerIndex = (event.getAction() &
+                        MotionEvent.ACTION_POINTER_INDEX_MASK) >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+                final int pointerId = event.getPointerId(pointerIndex);
+                if (pointerId == mActivePointerId) {
+                    // This was our active pointer going up. Choose a new
+                    // active pointer and adjust accordingly.
+                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+                    mActivePointerId = event.getPointerId(newPointerIndex);
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+
+                // Find the index of the active pointer and fetch its position
+                final int pointerIndexMove = event.findPointerIndex(mActivePointerId);
+                final float xMove = event.getX(pointerIndexMove);
+                final float yMove = event.getY(pointerIndexMove);
+
+                //from http://android-developers.blogspot.com/2010/06/making-sense-of-multitouch.html
+                // Calculate the distance moved
+                final float dx = xMove - aDownTouchX;
+                final float dy = yMove - aDownTouchY;
+
+
+                // Move the frame
+                aPosX += dx;
+                aPosY += dy;
+
+                //in this area would be code for doing something with the view as the frame moves.
+//                frame.setX(aPosX);
+                frame.setY(aPosY);
+                mFlingListener.onScroll(getScrollProgressPercent());
+                break;
+
+            case MotionEvent.ACTION_CANCEL: {
+                mActivePointerId = INVALID_POINTER_ID;
+                view.getParent().requestDisallowInterceptTouchEvent(false);
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    private float getScrollProgressPercent() {
+        if (movedBeyondTopBorder()) {
+            return -1f;
+        } else if (movedBeyondBottomBorder()) {
+            return 1f;
+        } else {
+            float zeroToOneValue = (aPosY + halfHeight - topBorder()) / (bottomBorder() - topBorder());
+            return zeroToOneValue * 2f - 1f;
+        }
+    }
+
+    private boolean resetCardViewOnStack() {
+        if(movedBeyondTopBorder()){
+            Log.i("Swipe ", "top");
+            onSelectedY(true, getExitPointX(-objectH), 100);
+            mFlingListener.onScroll(-1.0f);
+        } else if(movedBeyondBottomBorder()){
+            Log.i("Swipe ", "bottom");
+            onSelectedY(false, getExitPointX(parentHeight), 100);
+            mFlingListener.onScroll(1.0f);
+        }
+        else {
+            float abslMoveDistance = Math.abs(aPosX - objectX);
+            aPosX = 0;
+            aPosY = 0;
+            aDownTouchX = 0;
+            aDownTouchY = 0;
+            frame.animate()
+                    .setDuration(200)
+                    .setInterpolator(new OvershootInterpolator(1.5f))
+                    .x(objectX)
+                    .y(objectY)
+                    .rotation(0);
+            mFlingListener.onScroll(0.0f);
+            if (abslMoveDistance < 4.0) {
+                mFlingListener.onClick(dataObject);
+            }
+        }
+        return false;
+    }
+
+    private boolean movedBeyondBottomBorder() {
+        int centerX = (int) (frame.getX() + halfWidth);
+        int centerY = (int) (frame.getY() + halfHeight);
+        return (RECT_BOTTOM.contains(centerX, centerY) || (centerY > RECT_BOTTOM.bottom && RECT_BOTTOM.contains(centerX, RECT_BOTTOM.top)));
+//        return aPosY + halfHeight > bottomBorder();
+    }
+
+    private boolean movedBeyondTopBorder() {
+        int centerX = (int) (frame.getX() + halfWidth);
+        int centerY = (int) (frame.getY() + halfHeight);
+        return (RECT_TOP.contains(centerX, centerY) || (centerY < RECT_TOP.top && RECT_TOP.contains(centerX, 0)));
+//        return aPosY + halfHeight < topBorder();
+    }
+
+    private void onSelectedY(final boolean isTop, float exitX, int duration) {
+        isAnimationRunning = true;
+        float exitY;
+        if (isTop) {
+            exitY = -objectH;
+        } else {
+            exitY = parentHeight;
+        }
+
+        this.frame.animate()
+                .setDuration(duration)
+                .setInterpolator(new AccelerateInterpolator())
+                .x(exitX)
+                .y(exitY)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        if (isTop) {
+                            mFlingListener.onCardExited();
+                            mFlingListener.topExit(dataObject);
+                        } else {
+                            mFlingListener.onCardExited();
+                            mFlingListener.bottomExit(dataObject);
+                        }
+                        isAnimationRunning = false;
+                    }
+                });
+    }
+
+    private float getExitPointX(int exitYPoint) {
+        float[] x = new float[2];
+        x[0] = objectX;
+        x[1] = aPosX;
+
+        float[] y = new float[2];
+        y[0] = objectY;
+        y[1] = aPosY;
+
+        LinearRegression regression = new LinearRegression(y, x);
+
+        //Your typical y = ax+b linear regression
+        return (float) regression.slope() * exitYPoint + (float) regression.intercept();
+    }
+
+    public boolean isTouching() {
+        return this.mActivePointerId != INVALID_POINTER_ID;
+    }
+
+    public PointF getLastPoint() {
+        return new PointF(this.aPosX, this.aPosY);
+    }
+
+
+//    private float getRotationWidthOffset() {
+//        return objectW / MAX_COS - objectW;
+//    }
+
+    protected interface FlingListener {
+        void onCardExited();
+
+        void leftExit(Object dataObject);
+
+        void rightExit(Object dataObject);
+
+        void onClick(Object dataObject);
+
+        void onScroll(float scrollProgressPercent);
+
+        void topExit(Object dataObject);
+
+        void bottomExit(Object dataObject);
+    }
+}
---
+++ library/src/main/java/in/arjsna/swipecardlib/SwipeFlingPageView.java
@@ -0,0 +1,316 @@
+package in.arjsna.swipecardlib;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.PointF;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.FrameLayout;
+
+/**
+ * Created by arjun on 4/26/16.
+ */
+public class SwipeFlingPageView extends BaseFlingAdapterView {
+    private int MIN_ADAPTER_STACK = 6;
+    private int MAX_VISIBLE = 2;
+    private Adapter mAdapter;
+    private boolean mInLayout = false;
+    private int LAST_OBJECT_IN_STACK = 0;
+    private View mActiveCard;
+    private FlingPageListener flingPageListener;
+    private PointF mLastTouchPoint;
+    private int CURRENT_TRANSY_VAL;
+    private int CURRENT_SCALE_VAL;
+    private double SCALE_OFFSET = 0.04;
+    private int TRANS_OFFSET = 45;
+    private SwipeFlingPageView.OnPageFlingListener mFlingListener;
+    private OnItemClickListener mOnItemClickListener;
+
+    public SwipeFlingPageView(Context context) {
+        this(context, null);
+    }
+
+    public SwipeFlingPageView(Context context, AttributeSet attrs) {
+        this(context, attrs, R.styleable.SwipeFlingPageView_SwipeFlingStyle);
+    }
+
+    public SwipeFlingPageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SwipeFlingPageView, defStyle, 0);
+//        MAX_VISIBLE = a.getInt(R.styleable.SwipeFlingCardView_max_visible, MAX_VISIBLE);
+        MIN_ADAPTER_STACK = a.getInt(R.styleable.SwipeFlingCardView_min_adapter_stack, MIN_ADAPTER_STACK);
+        a.recycle();
+    }
+
+    @Override
+    public void requestLayout() {
+        if (!mInLayout) {
+            super.requestLayout();
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        // if we don't have an adapter, we don't need to do anything
+        if (mAdapter == null) {
+            return;
+        }
+
+        mInLayout = true;
+        final int adapterCount = mAdapter.getCount();
+
+        if(adapterCount == 0) {
+            removeAllViewsInLayout();
+        }else {
+            View topCard = getChildAt(LAST_OBJECT_IN_STACK);
+            if(mActiveCard!=null && topCard!=null && topCard==mActiveCard) {
+                if (this.flingPageListener.isTouching()) {
+                    PointF lastPoint = this.flingPageListener.getLastPoint();
+                    if (this.mLastTouchPoint == null || !this.mLastTouchPoint.equals(lastPoint)) {
+                        this.mLastTouchPoint = lastPoint;
+                        removeViewsInLayout(0, LAST_OBJECT_IN_STACK);
+                        layoutChildren(1, adapterCount);
+                    }
+                }
+            }else{
+                // Reset the UI and set top view listener
+                removeAllViewsInLayout();
+                layoutChildren(0, adapterCount);
+                setTopView();
+            }
+        }
+
+        mInLayout = false;
+
+        if(adapterCount <= MIN_ADAPTER_STACK) mFlingListener.onAdapterAboutToEmpty(adapterCount);
+    }
+
+    private void layoutChildren(int startingIndex, int adapterCount){
+        resetOffsets();
+        if(adapterCount < MAX_VISIBLE){
+            MAX_VISIBLE = adapterCount;
+        }
+        while (startingIndex < MAX_VISIBLE) {
+            View newUnderChild = mAdapter.getView(startingIndex, null, this);
+            if (newUnderChild.getVisibility() != GONE) {
+                makeAndAddView(newUnderChild, false);
+//                LAST_OBJECT_IN_STACK = startingIndex;
+            }
+            startingIndex++;
+        }
+
+        /**
+         * This is to add a base view at end. To make an illusion that views come out from
+         * a base card. The scale and translation of this view is same as the one previous to
+         * this.
+         */
+        if(startingIndex >= adapterCount){
+            LAST_OBJECT_IN_STACK = --startingIndex;
+            return;
+        }
+        View newUnderChild = mAdapter.getView(startingIndex, null, this);
+        if (newUnderChild != null && newUnderChild.getVisibility() != GONE) {
+            makeAndAddView(newUnderChild, true);
+            LAST_OBJECT_IN_STACK = startingIndex;
+        }
+    }
+
+    private void resetOffsets() {
+        CURRENT_TRANSY_VAL = 0;
+        CURRENT_SCALE_VAL = 0;
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void makeAndAddView(View child, boolean isBase) {
+
+        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) child.getLayoutParams();
+        if(isBase){
+            child.setScaleX((float) (child.getScaleX() - (CURRENT_SCALE_VAL - SCALE_OFFSET)));
+            child.setScaleY((float) (child.getScaleY() - (CURRENT_SCALE_VAL - SCALE_OFFSET)));
+            child.setY(child.getTranslationY() + CURRENT_TRANSY_VAL - TRANS_OFFSET);
+        } else {
+            child.setScaleX(child.getScaleX() - CURRENT_SCALE_VAL);
+            child.setScaleY(child.getScaleY() - CURRENT_SCALE_VAL);
+            child.setY(child.getTranslationY() + CURRENT_TRANSY_VAL);
+        }
+        CURRENT_SCALE_VAL += SCALE_OFFSET;
+        CURRENT_TRANSY_VAL += TRANS_OFFSET;
+        addViewInLayout(child, 0, lp, true);
+
+        final boolean needToMeasure = child.isLayoutRequested();
+        if (needToMeasure) {
+            int childWidthSpec = getChildMeasureSpec(getWidthMeasureSpec(),
+                    getPaddingLeft() + getPaddingRight() + lp.leftMargin + lp.rightMargin,
+                    lp.width);
+            int childHeightSpec = getChildMeasureSpec(getHeightMeasureSpec(),
+                    getPaddingTop() + getPaddingBottom() + lp.topMargin + lp.bottomMargin,
+                    lp.height);
+            child.measure(childWidthSpec, childHeightSpec);
+        } else {
+            cleanupLayoutState(child);
+        }
+
+
+        int w = child.getMeasuredWidth();
+        int h = child.getMeasuredHeight();
+
+        int gravity = lp.gravity;
+        if (gravity == -1) {
+            gravity = Gravity.TOP | Gravity.START;
+        }
+
+
+        int layoutDirection = getLayoutDirection();
+        final int absoluteGravity = Gravity.getAbsoluteGravity(gravity, layoutDirection);
+        final int verticalGravity = gravity & Gravity.VERTICAL_GRAVITY_MASK;
+
+        int childLeft;
+        int childTop;
+        switch (absoluteGravity & Gravity.HORIZONTAL_GRAVITY_MASK) {
+            case Gravity.CENTER_HORIZONTAL:
+                childLeft = (getWidth() + getPaddingLeft() - getPaddingRight()  - w) / 2 +
+                        lp.leftMargin - lp.rightMargin;
+                break;
+            case Gravity.END:
+                childLeft = getWidth() + getPaddingRight() - w - lp.rightMargin;
+                break;
+            case Gravity.START:
+            default:
+                childLeft = getPaddingLeft() + lp.leftMargin;
+                break;
+        }
+        switch (verticalGravity) {
+            case Gravity.CENTER_VERTICAL:
+                childTop = (getHeight() + getPaddingTop() - getPaddingBottom()  - h) / 2 +
+                        lp.topMargin - lp.bottomMargin;
+                break;
+            case Gravity.BOTTOM:
+                childTop = getHeight() - getPaddingBottom() - h - lp.bottomMargin;
+                break;
+            case Gravity.TOP:
+            default:
+                childTop = getPaddingTop() + lp.topMargin;
+                break;
+        }
+
+        child.layout(childLeft, childTop, childLeft + w, childTop + h);
+    }
+
+    private void setTopView() {
+        if(getChildCount()>0){
+
+            mActiveCard = getChildAt(LAST_OBJECT_IN_STACK);
+            if(mActiveCard!=null) {
+
+                flingPageListener = new FlingPageListener(mActiveCard, mAdapter.getItem(0),
+                        new FlingPageListener.FlingListener() {
+
+                            @Override
+                            public void onCardExited() {
+                                mActiveCard = null;
+                                mFlingListener.removeFirstObjectInAdapter();
+                            }
+
+                            @Override
+                            public void leftExit(Object dataObject) {
+                                mFlingListener.onLeftCardExit(dataObject);
+                            }
+
+                            @Override
+                            public void rightExit(Object dataObject) {
+                                mFlingListener.onRightCardExit(dataObject);
+                            }
+
+                            @Override
+                            public void onClick(Object dataObject) {
+                                if(mOnItemClickListener!=null)
+                                    mOnItemClickListener.onItemClicked(0, dataObject);
+
+                            }
+
+                            @Override
+                            public void onScroll(float scrollProgressPercent) {
+                                Log.i("Scroll Percentage ", scrollProgressPercent + "");
+                                mFlingListener.onScroll(scrollProgressPercent);
+                                int childCount = getChildCount() - 1;
+                                if(childCount < MAX_VISIBLE){
+                                    while (childCount > 0) {
+                                        relayoutChild(getChildAt(childCount - 1), Math.abs(scrollProgressPercent), childCount);
+                                        childCount--;
+                                    }
+                                } else {
+                                    while (childCount > 1) {
+                                        relayoutChild(getChildAt(childCount - 1), Math.abs(scrollProgressPercent), childCount - 1);
+                                        childCount--;
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void topExit(Object dataObject) {
+                                mFlingListener.onTopCardExit(dataObject);
+                            }
+
+                            @Override
+                            public void bottomExit(Object dataObject) {
+                                mFlingListener.onBottomCardExit(dataObject);
+                            }
+                        });
+
+                mActiveCard.setOnTouchListener(flingPageListener);
+            }
+        }
+    }
+
+    public void relayoutChild(View child, float scrollDis, int childcount){
+        float absScrollDis = scrollDis > 1 ? 1 : scrollDis;
+        float newScale = (float) (1 - SCALE_OFFSET * (MAX_VISIBLE - childcount) + absScrollDis * SCALE_OFFSET);
+        child.setScaleX(newScale);
+        child.setScaleY(newScale);
+        child.setTranslationY(TRANS_OFFSET * (MAX_VISIBLE - childcount) - absScrollDis * TRANS_OFFSET);
+    }
+
+    public interface OnItemClickListener {
+        void onItemClicked(int itemPosition, Object dataObject);
+    }
+
+    @Override
+    public Adapter getAdapter() {
+        return null;
+    }
+
+    @Override
+    public void setAdapter(Adapter adapter) {
+
+    }
+
+    @Override
+    public View getSelectedView() {
+        return null;
+    }
+
+    public void setFlingListener(OnPageFlingListener OnCardFlingListener) {
+        this.mFlingListener = OnCardFlingListener;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener){
+        this.mOnItemClickListener = onItemClickListener;
+    }
+
+    public interface OnPageFlingListener {
+        void removeFirstObjectInAdapter();
+        void onLeftCardExit(Object dataObject);
+        void onRightCardExit(Object dataObject);
+        void onAdapterAboutToEmpty(int itemsInAdapter);
+        void onScroll(float scrollProgressPercent);
+
+        void onTopCardExit(Object dataObject);
+        void onBottomCardExit(Object dataObject);
+    }
+}
